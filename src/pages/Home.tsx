8703c9b14d029f911bda3c137953c893f70f149e
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-20 23:43:40 -0300

    Created function remove Skill

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,6 +43,12 @@ export function Home() {
     setMySkills([...mySkills, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ))
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Daniel</Text>
@@ -56,14 +62,20 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} title="Add"/>
+      <Button onPress={handleAddNewSkill} title="Add" />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skill</Text>
 
       <FlatList
         data={mySkills}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SkillCard skill={item.name} />}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )
+        }
       />
     </View>
   );
